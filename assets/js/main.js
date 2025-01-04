eaf8f2eb61f60cd5c79c293a8ecc2c41b66a51e2
Tafita <142479482+Tafita56@users.noreply.github.com>
2025-01-04 16:15:41 +0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -99,9 +99,9 @@
     new Typed('.typed', {
       strings: typed_strings,
       loop: true,
-      typeSpeed: 100,
-      backSpeed: 50,
-      backDelay: 2000
+      typeSpeed: 50,
+      backSpeed: 25,
+      backDelay: 1000
     });
   }
 
